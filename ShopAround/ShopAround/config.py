7f167e6ce874190c2d380fa885aabf734f7a986c
Titanxz <951776550@qq.com>
2019-12-20 21:21:19 +0800

    增加配置文件

---
+++ ShopAround/ShopAround/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @Author  :   {Titanxz}
+# @Time    :   2019/12/19 18:07:37
+# @License :   (C) Copyright 2019, {python_1904}
+
+username = 'your mysql name'
+pwd = 'your mysql password'
+
+
